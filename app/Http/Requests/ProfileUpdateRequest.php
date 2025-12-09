9ee1d4fd129fe76f268ec8aff663ee81e924f5e5
Sebastian Mancilla Londoño <sebasti.m.0310@gmail.com>
2025-12-09 15:50:05 -0500

    Modificación #1 Gestion de Usuarios (#2)
    * Actualización de la migracion de la tabla users
    
    * Añadiendo los nuevos campos de la tabla users a su controlador y modelo
    
    * Actualizacion de reglas de validacion para campos de la tabla users
    
    * Se añade la configuracion del idioma español para breeze
    
    * Actualizacion de vistas en idioma español, modificación en formularios con nuevos campos
    
    * Merge branch 'driveloop7-art:main' into main

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -16,7 +16,8 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'nomusu' => ['required', 'string', 'max:255'],
+            'apeusu' => ['required', 'string', 'max:255'],
             'email' => [
                 'required',
                 'string',
